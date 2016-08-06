330f5fde048dd1df8e5169ee987649987292da9a
Chris Karr <github@audacious-software.com>
2016-08-06 13:39:52 -0500

    Initial template code check-in

---
+++ iNSite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iNSite
+//
+//  Created by Chris Karr on 8/6/16.
+//  Copyright © 2016 iNSite AEC Hackathon Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iNSite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iNSite
+//
+//  Created by Chris Karr on 8/6/16.
+//  Copyright © 2016 iNSite AEC Hackathon Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
